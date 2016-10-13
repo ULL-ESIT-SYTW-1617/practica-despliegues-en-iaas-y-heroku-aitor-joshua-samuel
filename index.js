6b6e5bbfe4f0a79cb38754aebe153c043162b8b6
Samuel Ramos <alu0100815146@ull.edu.es>
2016-10-13 10:16:40 +0100

    Añadiendo procfile

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var exec = require('child_process').exec;
 
 
 
@@ -8,8 +9,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/gh-pages'));
 
-
-
+app.get('/sync', (request, response) => {
+  function puts(error, stdout, stderr) { console.log(stdout) }
+  exec("git pull", puts);
+});
 
 app.listen(app.get('port'), () => {
     console.log(`Node app is running at localhost: ${app.get('port')}` );
